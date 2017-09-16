bf3bf387c2294216bdc081a6221d8a6c10b63975
laigaaming <laigaaming@163.com>
2017-09-17 00:19:17 +0800

    Twilio Send SMS.

--- src/pl/movies/media.py
+++ src/pl/movies/media.py
@@ -1,2 +1,12 @@
+import webbrowser
+
 class Movie(): # Class name first letter is uppercase: Google style guide for Python - https://google.github.io/styleguide/pyguide.html
 
+    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
